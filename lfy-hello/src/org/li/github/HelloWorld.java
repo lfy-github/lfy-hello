05063d4af6559fda471e782dbb153f42a710ce14
lfy-github <1796807641@qq.com>
2016-07-09 08:28:07 +0800

    update by lfy 0709-01

--- lfy-hello/src/org/li/github/HelloWorld.java
+++ lfy-hello/src/org/li/github/HelloWorld.java
@@ -2,10 +2,6 @@ package org.li.github;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("程序猿、妹纸、购物狂……");
-		System.out.println("程序猿、妹纸、购物狂……");
-		System.out.println("程序猿、妹纸、购物狂……");
-		System.out.println("edit by remote 程序猿、妹纸、购物狂……");
-		System.out.println("edit by remote 程序猿、妹纸、购物狂……");
+		System.out.println("hello()......");
 	}
 }
